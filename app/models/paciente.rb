a34f7bf1d59cd6fdaf8c0d53f7717aa96d63e29a
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-10-05 09:14:48 -0300

    Correçoes em Paciente

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,5 +1,4 @@
 class Paciente < ApplicationRecord
-    has_many :consultas, dependent: :destroy
     has_many :enderecos, dependent: :destroy
     
     accepts_nested_attributes_for :enderecos, allow_destroy: true, reject_if: :all_blank
