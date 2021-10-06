12757613a8b60a86609de893b2aa986580101b9f
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-10-06 13:06:28 -0300

    Validates de Horario + correçoes em medico e paciente

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -1,12 +1,20 @@
 class Consultum < ApplicationRecord
+  require 'Time'
   belongs_to :medico
   belongs_to :paciente
 
+  validate :validarHorario
   validate :validarDataConsulta
 
   def validarDataConsulta
     if data.present? && data < Date.today
       errors.add(:data, "Invalid Date")
+    end
+  end
+
+  def validarHorario
+    if data.present? && Date.today && horario < Time.current
+      errors.add(:horario, "Invalid Hour")
     end
   end
 end
