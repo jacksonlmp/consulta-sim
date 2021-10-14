eb789ad12f3cb1367daf0b8f24c231f3c1a49660
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-10-13 21:28:45 -0300

    Validations de Paciente e medico

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,7 +1,7 @@
 class Medico < ApplicationRecord
     validates :nomeCompleto, presence: true, length: {minimum: 3, too_short: "Must have at least 3 characters "}
     validates :cpf, length: {is: 11}, presence: true, uniqueness: true,numericality: { only_integer: true }
-    validates :email, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :especialidade, presence: true
     validates :crm, presence: true, uniqueness: true
     validates :dataNascimento, presence: true
--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -6,7 +6,7 @@ class Paciente < ApplicationRecord
     #Validates
     validates :nomeCompleto, presence: true, length: {minimum: 5, too_short: "Must have at least 5 characteres "}
     validates :cpf, length: {is: 11}, presence: true, uniqueness: true, numericality: { only_integer: true }
-    validates :email, presence: true, uniqueness: true, length: {minimum: 5, too_short: "Must have at least 5 characteres ", maximum: 50, too_long: "Must have at maximum 50 characters" }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: {minimum: 5, too_short: "Must have at least 5 characteres ", maximum: 50, too_long: "Must have at maximum 50 characters" }
     validates :dataNascimento, presence: true
     validates :enderecos, presence: true
     validate :validarDataNascimento   
