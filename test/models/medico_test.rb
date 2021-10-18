f62e662c721a24b1070e0ecff62a5d4ed865e777
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-10-18 20:09:56 -0300

    Update no teste de medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -18,9 +18,9 @@ class MedicoTest < ActiveSupport::TestCase
   end
 
   test "editando medico sem cpf" do
-    medico = Medico.new(nomeCompleto: 'Paulo Muzy', dataNascimento: '1969-05-08', cpf: '', email: 'muzy@paulo.br', especialidade: 'Endocrinologista', crm: '46584')
-    assert medico.save 
-    assert_not medico.update(nomeCompleto: 'Paulo Muzy ', dataNascimento: '1969-05-08', cpf: '85295175346', email: 'muzy@paulo.br', especialidade: 'Endocrinologista', crm: '46584')
+    medico = Medico.new(nomeCompleto: 'Paulo Muzy', dataNascimento: '1969-05-08', cpf: '85295175346', email: 'muzy@paulo.br', especialidade: 'Endocrinologista', crm: '46584')
+
+    assert_not medico.update(nomeCompleto: 'Paulo Muzy ', dataNascimento: '1969-05-08', cpf: '', email: 'muzy@paulo.br', especialidade: 'Endocrinologista', crm: '46584')
   end
   
 end
